8af5a3b28760fa7e7fccc7cb21efda2685594c85
Xappiens <xappiens@xappiens.com>
2025-05-17 20:23:45 +0000

    Actualización automática: 2025-05-17_20-23-45

--- src/config/version.ts
+++ src/config/version.ts
@@ -2,6 +2,6 @@ export const VERSION = {
   major: 1,
   minor: 0,
   patch: 0,
-  build: 1new Date().getTime(),
+  build: 2,
   toString: () => `v${VERSION.major}.${VERSION.minor}.${VERSION.patch}`
 }; 
